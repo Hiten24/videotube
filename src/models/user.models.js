4301369e94332159626331a183db6e67a704ae44
Hiten Chawda <hitenchawda24@gmail.com>
2025-03-14 18:15:09 +0000

    Hooks and methods in moongoose with JWT

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
     {
@@ -46,5 +48,42 @@ const userSchema = new Schema(
     },
     { timestamps: true }
 )
+
+userSchema.pre("save", async function (next) {
+    
+    if(!this.modified("password")) return next()
+
+    this.password = bcrypt.hash(this.password, 10)
+
+    next()
+})
+
+userSchema.method.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.method.generateAccessToken = function () {
+    // short lived access token
+    // on production we only put _id and then fetch the info through id
+    jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullname: this.fullname
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY })
+}
+
+userSchema.method.generateRefreshToken = function () {
+    // short lifved access token
+    return jwt.sign({
+        _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY })
+}
+
+
 
 export const User = mongoose.model("User", userSchema)
