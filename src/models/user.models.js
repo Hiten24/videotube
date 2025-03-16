ecfd8a4a863678517db138e63bf04ddfb8c7510d
Hiten Chawda <hitenchawda24@gmail.com>
2025-03-16 13:40:19 +0000

    fixed methods in syntex in user.models.js

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -58,11 +58,11 @@ userSchema.pre("save", async function (next) {
     next()
 })
 
-userSchema.method.isPasswordCorrect = async function (password) {
+userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password)
 }
 
-userSchema.method.generateAccessToken = function () {
+userSchema.methods.generateAccessToken = function () {
     // short lived access token
     // on production we only put _id and then fetch the info through id
     jwt.sign({
@@ -75,7 +75,7 @@ userSchema.method.generateAccessToken = function () {
     { expiresIn: process.env.ACCESS_TOKEN_EXPIRY })
 }
 
-userSchema.method.generateRefreshToken = function () {
+userSchema.methods.generateRefreshToken = function () {
     // short lifved access token
     return jwt.sign({
         _id: this._id,
