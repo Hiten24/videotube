ef80649ac66b0f0a3b519e5d29a1d5c77b5a174e
Hiten Chawda <hitenchawda24@gmail.com>
2025-03-16 18:28:33 +0000

    created the blueprint of next apis

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 8001
 connectDB()
     .then(() => {
         app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`)
+            console.log(`⚙️ Server is running on port ${PORT}`)
         })
     })
     .catch((err) => {
